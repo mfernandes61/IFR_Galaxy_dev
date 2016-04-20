4350a733dd3bb5202065cebe1656d63e9cc00573
mfernandes61 <mfernandes61@gmail.com>
2016-04-20 15:17:58 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run notebook server as public server on 'standard' port and do not run a local browser.
-/usr/bin/ipython notebook --ip=0.0.0.0 --no-browser --port=8888
+/usr/bin/ipython notebook --ip=0.0.0.0 --no-browser --port=8888 &
 #
 # Now run other software
 # ** IN this case Galaxy Web Server **
